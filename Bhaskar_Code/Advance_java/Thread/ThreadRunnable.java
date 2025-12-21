c0b6b1d5fcd6110f72c019fd929705de1fb506a4
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-21 17:08:07 +0530

    Runnable Interface in Thread

---
+++ Bhaskar_Code/Advance_java/Thread/ThreadRunnable.java
@@ -0,0 +1,32 @@
+package Thread;
+
+public class ThreadRunnable {
+    public static void main(String[] args) {
+        Runnable obj1 = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Runnable obj2 = () -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1 = new Thread(obj2);
+        Thread t2 = new Thread(obj1);
+
+        t1.start();
+        t2.start();
+    }
+}
