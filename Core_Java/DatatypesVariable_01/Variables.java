764bf0a4a4d7e4f7b90b12c13c8326c007d55feb
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-21 11:06:13 +0530

    Java interview preparation

--- Core_Java/DatatypesVariable_01/Variables.java
+++
@@ -1,8 +0,0 @@
-class Variables{
-    public static void main (String args[]){
-        int number1=5;
-        int number2=3;
-        int result= number1+number2;
-        System.out.println("The result is :"+result);
-    } 
-}
--- Core_Java/conditional_03/Ternary.java
+++
@@ -1,8 +0,0 @@
-class Ternary {
-    public static void main(String[] args) {
-        int number = 5;
-        int result = (number % 2 == 0) ? 10 : 20;
-
-        System.out.println(result);
-    }
-}
