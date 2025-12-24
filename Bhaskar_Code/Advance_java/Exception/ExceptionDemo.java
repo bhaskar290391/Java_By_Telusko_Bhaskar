12ff67ec699193767d3bcfe43742af591ae524dc
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-24 20:06:54 +0530

    Exception Demo

---
+++ Bhaskar_Code/Advance_java/Exception/ExceptionDemo.java
@@ -0,0 +1,16 @@
+class ExceptionDemo{
+    public static void main(String[] args) {
+        
+        int i=0;
+        int j=0;
+
+        try {
+            j=15/i;
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+
+        System.out.println(j);
+        System.out.println("bye");
+    }
+}
