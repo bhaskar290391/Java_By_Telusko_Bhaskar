3c0f02f3dca7080fe0547859447a7cae9c60b9b1
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-25 13:06:17 +0530

    Inheritance demo

---
+++ Bhaskar_Code/Core_Java/OOPS/Inheritance/InheritanceDemo.java
@@ -0,0 +1,31 @@
+class Calc{
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+
+    public int sub(int n1,int n2){
+        return n1-n2;
+    }
+}
+
+class AdvanceCalc extends Calc{
+    public int multi(int n1,int n2){
+        return n1*n2;
+    }
+
+    public int div(int n1,int n2){
+        return n1/n2;
+    }
+}
+public class InheritanceDemo{
+    public static void main(String[] args) {
+        AdvanceCalc adv=new AdvanceCalc();
+
+        int r1=adv.add(4, 4);
+                int r2=adv.sub(5, 4);
+                        int r3=adv.multi(4, 8);
+                                int r4=adv.add(10, 4);
+
+    System.out.println(r1+" : "+r2+" : "+ r3+" : "+ r4);
+    }
+}
