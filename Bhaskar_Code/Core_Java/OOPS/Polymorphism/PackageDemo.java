5088b861cdeb15d49f64519dd93f689a17a4931a
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-25 16:12:37 +0530

    Package in java

---
+++ Bhaskar_Code/Core_Java/OOPS/Polymorphism/PackageDemo.java
@@ -0,0 +1,13 @@
+import tools.*;
+
+
+public class PackageDemo{
+    public static void main(String[] args) {
+        
+        AdvanceCalculator obj=new AdvanceCalculator();
+        int result=obj.add(5, 5);
+
+        System.out.println("The result ==>"+result);
+
+           }
+}
---
+++ Bhaskar_Code/Core_Java/OOPS/Polymorphism/tools/Calculater.java
@@ -0,0 +1,8 @@
+package tools;
+
+class Calculater{
+    public int add(int n1,int n2){
+        return n1+n2;
+    }
+
+}
