f2e127af40182d56a8f49e30ce6eacf1c4eeed4a
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-27 15:43:45 +0530

    Qualifier and Primary

--- Bhaskar_Code/SpringProject/Spring-java-based-configuration/src/main/java/com/bhaskar/spring/app/config/AppConfig.java
+++ Bhaskar_Code/SpringProject/Spring-java-based-configuration/src/main/java/com/bhaskar/spring/app/config/AppConfig.java
@@ -3,14 +3,19 @@ package com.bhaskar.spring.app.config;
 import com.bhaskar.spring.app.Alien;
 import com.bhaskar.spring.app.Computer;
 import com.bhaskar.spring.app.Desktop;
+import com.bhaskar.spring.app.Laptop;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    //public Alien alien(@Autowired @Qualifier("desktop") Computer com){
     public Alien alien(Computer com){
         Alien alien =new Alien();
         alien.setAge(29);
@@ -24,5 +29,11 @@ public class AppConfig {
     //@Scope("prototype")
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop(){
+        return  new Laptop();
     }
 }
