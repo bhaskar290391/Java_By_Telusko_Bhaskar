9aa22dc4392139410d07a43ee9e2f58935aa8a85
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-27 13:27:30 +0530

    getting bean by type

--- Bhaskar_Code/SpringProject/Spring1/src/main/java/com/bhaskar/spring/app/App.java
+++ Bhaskar_Code/SpringProject/Spring1/src/main/java/com/bhaskar/spring/app/App.java
@@ -12,7 +12,14 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context= new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj= (Alien) context.getBean("alien");
+        //Getting object by bean id which provide object
+        //Alien obj= (Alien) context.getBean("alien");
+
+
+        //getting object by type
+        Alien obj=context.getBean("alien",Alien.class);
+
+
         System.out.println("Age ==> " +obj.getAge());
         System.out.println("Salary ==>"+obj.getSalary());
         obj.code();
@@ -22,5 +29,7 @@ public class App
 //        System.out.println(obj1.age);
 //
 //        System.out.println( "Hello World!" );
+
+        Desktop d=context.getBean(Desktop.class);
     }
 }
