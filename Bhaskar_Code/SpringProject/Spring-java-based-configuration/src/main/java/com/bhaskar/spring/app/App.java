144bb85d1a58bd5e2448b29acef4788bd38c2ef4
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-27 15:25:24 +0530

    Bean name

--- Bhaskar_Code/SpringProject/Spring-java-based-configuration/src/main/java/com/bhaskar/spring/app/App.java
+++ Bhaskar_Code/SpringProject/Spring-java-based-configuration/src/main/java/com/bhaskar/spring/app/App.java
@@ -16,7 +16,7 @@ public class App
 
     ApplicationContext context= new AnnotationConfigApplicationContext(AppConfig.class);
 
-    Desktop desktop = context.getBean(Desktop.class);
+    Desktop desktop = context.getBean("beast",Desktop.class);
     desktop.compile();
 
 
--- Bhaskar_Code/SpringProject/Spring-java-based-configuration/src/main/java/com/bhaskar/spring/app/config/AppConfig.java
+++ Bhaskar_Code/SpringProject/Spring-java-based-configuration/src/main/java/com/bhaskar/spring/app/config/AppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean(name = {"com1","beast","desktop1"})
     @Bean
     public Desktop desktop(){
         return new Desktop();
