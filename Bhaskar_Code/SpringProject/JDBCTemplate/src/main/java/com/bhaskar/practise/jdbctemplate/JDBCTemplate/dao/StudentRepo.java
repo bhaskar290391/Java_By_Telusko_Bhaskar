9f88feb6471d83ed01ae9cc25e7248a4faffd345
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-28 21:17:29 +0530

    adding schema and data sql and logic for saving

--- Bhaskar_Code/SpringProject/JDBCTemplate/src/main/java/com/bhaskar/practise/jdbctemplate/JDBCTemplate/dao/StudentRepo.java
+++ Bhaskar_Code/SpringProject/JDBCTemplate/src/main/java/com/bhaskar/practise/jdbctemplate/JDBCTemplate/dao/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.bhaskar.practise.jdbctemplate.JDBCTemplate.dao;
 
 import com.bhaskar.practise.jdbctemplate.JDBCTemplate.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,8 +10,18 @@ import java.util.List;
 
 @Repository
 public class StudentRepo {
+
+    @Autowired
+    private JdbcTemplate template ;
+
     public void save(Student s1) {
-        System.out.println("Added");
+
+        String query="insert into student (rollno, name , marks) values (?,?,?)";
+
+        int row =template.update(query,s1.getRollNo(),s1.getName(),s1.getMarks());
+        System.out.println("The row affected by using insert "+ row);
+
+
     }
 
 
---
+++ Bhaskar_Code/SpringProject/JDBCTemplate/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollno, name , marks) values (101,'bhaskar',85);
+insert into student (rollno, name , marks) values (102,'Kanishk',92);
+insert into student (rollno, name , marks) values (103,'sammy',75);
