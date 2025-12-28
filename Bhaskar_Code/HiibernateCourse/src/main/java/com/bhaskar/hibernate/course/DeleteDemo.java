ae9b24da80698b0933213286e8a7fd425c958540
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-28 12:45:21 +0530

    Update and delete using hibernate

--- Bhaskar_Code/HiibernateCourse/src/main/java/com/bhaskar/hibernate/course/DeleteDemo.java
+++ Bhaskar_Code/HiibernateCourse/src/main/java/com/bhaskar/hibernate/course/DeleteDemo.java
@@ -6,16 +6,21 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
-public class FetchDemo {
+public class DeleteDemo {
     public static void main() {
-        Student student =new Student();
+
         SessionFactory sessionFactory=new Configuration()
                 .addAnnotatedClass(Student.class)
                 .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
 
         Session session= sessionFactory.openSession();
-        student=session.find(Student.class,2021);
+        Student student=session.find(Student.class,1992);
+
+        Transaction transaction =session.beginTransaction();
+        session.remove(student);
+        transaction.commit();
+
         session.close();
         sessionFactory.close();
         System.out.println(student);
--- Bhaskar_Code/HiibernateCourse/src/main/java/com/bhaskar/hibernate/course/UpdateDemo.java
+++ Bhaskar_Code/HiibernateCourse/src/main/java/com/bhaskar/hibernate/course/UpdateDemo.java
@@ -6,23 +6,28 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
-public class Main {
+public class UpdateDemo {
     public static void main() {
 
         Student student=new Student();
-        student.setAge(27);
+        student.setAge(33);
         student.setSname("Soni");
         student.setRollNo(1992);
 
+        Student s1=new Student();
+        student.setAge(29);
+        student.setSname("Babu");
+        student.setRollNo(1995);
+
         SessionFactory sessionFactory=new Configuration()
-                .addAnnotatedClass(com.bhaskar.hibernate.course.Student.class)
+                .addAnnotatedClass(Student.class)
                 .configure("hibernate.cfg.xml")
                 .buildSessionFactory();
 
         Session session= sessionFactory.openSession();
 
         Transaction transaction =session.beginTransaction();
-        session.persist(student);
+        session.merge(s1);
         transaction.commit();
         session.close();
         sessionFactory.close();
