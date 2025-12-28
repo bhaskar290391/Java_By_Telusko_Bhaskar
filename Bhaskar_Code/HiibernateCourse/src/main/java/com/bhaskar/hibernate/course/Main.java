b12ed493820831e005e625416e4f0ab6a23095d1
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-28 10:16:18 +0530

    Code refactor

--- Bhaskar_Code/HiibernateCourse/src/main/java/com/bhaskar/hibernate/course/Main.java
+++ Bhaskar_Code/HiibernateCourse/src/main/java/com/bhaskar/hibernate/course/Main.java
@@ -10,21 +10,22 @@ public class Main {
     public static void main() {
 
         Student student=new Student();
-        student.setAge(10);
-        student.setSname("Kanishk");
-        student.setRollNo(2021);
+        student.setAge(27);
+        student.setSname("Soni");
+        student.setRollNo(1992);
 
-        Configuration cfg=new Configuration();
-        cfg.configure("hibernate.cfg.xml");
-        cfg.addAnnotatedClass(com.bhaskar.hibernate.course.Student.class);
+        SessionFactory sessionFactory=new Configuration()
+                .addAnnotatedClass(com.bhaskar.hibernate.course.Student.class)
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
 
-        SessionFactory sessionFactory=cfg.buildSessionFactory();
         Session session= sessionFactory.openSession();
 
         Transaction transaction =session.beginTransaction();
         session.persist(student);
         transaction.commit();
-
+        session.close();
+        sessionFactory.close();
         System.out.println(student);
     }
 }
