683cdfe67218e42be7849d90696d575d4c1f4912
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-29 13:17:53 +0530

    Thymeleaf demo

---
+++ Bhaskar_Code/SpringProject/SprintBootWebMVCUsingThymeLeaf/src/main/java/com/bhaskar/boot/web/mvc/HomeController.java
@@ -0,0 +1,60 @@
+package com.bhaskar.boot.web.mvc;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @ModelAttribute("course")
+    public String course(){
+        return "java";
+    }
+
+
+    @RequestMapping("/")
+    public String home(){
+        System.out.println("Home page called");
+        return "index";
+    }
+
+    /*
+    @RequestMapping("/add")
+    public String add(HttpServletRequest request, HttpSession session){
+        int num1= Integer.parseInt(request.getParameter("num1"));
+        int num2= Integer.parseInt(request.getParameter("num2"));
+
+        int result= num1+num2;
+
+        session.setAttribute("result",result);
+        System.out.println(result);
+        return "result.html";
+    }
+*/
+
+
+    @RequestMapping("/add")
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
+
+        int result= num1+num2;
+
+      mv.addObject("result",result);
+      mv.setViewName("result");
+
+      return mv;
+    }
+
+
+    @RequestMapping("/addAlien")
+    public String add(@ModelAttribute  Alien alien){
+        return "result";
+    }
+
+}
---
+++ Bhaskar_Code/SpringProject/SprintBootWebMVCUsingThymeLeaf/src/main/java/com/bhaskar/boot/web/mvc/SprintBootWebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.bhaskar.boot.web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootWebMvcApplication.class, args);
+	}
+
+}
