2793f1739e8f81fc1c0f2ae371abb8004810b3c8
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-29 12:20:36 +0530

    model add aatributes

--- Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/java/com/bhaskar/boot/web/mvc/HomeController.java
+++ Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/java/com/bhaskar/boot/web/mvc/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,11 +33,11 @@ public class HomeController {
 
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int num1,@RequestParam("num2") int num2, HttpSession session){
-        
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model){
+
         int result= num1+num2;
 
-        session.setAttribute("result",result);
+        model.addAttribute("result",result);
         System.out.println(result);
         return "result.jsp";
     }
--- Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/webapp/result.jsp
+++ Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/webapp/result.jsp
@@ -4,7 +4,7 @@
 <link rel="stylesheet"  type="text/css" href="style.css" />
 </head>
     <Body>
-        <h2>Result : <%= session.getAttribute("result") %></h2>
+        
         <h2>Result : ${result}</h2>
 
     </Body>
