06bbdce9ca30ebefab948c414fef9c50413d5341
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-29 11:19:21 +0530

    Creating mvc with spring boot

---
+++ Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@page language="java" %>
+<HTML>
+    <Body>
+        <H1>Hello World !!!! <H1>
+    </Body>
+</HTML>
---
+++ Bhaskar_Code/SpringProject/SprintBootWebMVC/src/test/java/com/bhaskar/boot/web/mvc/SprintBootWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskar.boot.web.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
