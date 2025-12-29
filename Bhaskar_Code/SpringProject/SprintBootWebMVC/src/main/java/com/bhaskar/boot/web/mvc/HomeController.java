e7ee2f53d7aeb89ed60146130abac848dfae5567
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-29 12:05:46 +0530

    setting session attribute

--- Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/java/com/bhaskar/boot/web/mvc/HomeController.java
+++ Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/java/com/bhaskar/boot/web/mvc/HomeController.java
@@ -1,5 +1,8 @@
 package com.bhaskar.boot.web.mvc;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,4 +14,18 @@ public class HomeController {
         System.out.println("Home page called");
         return "index.jsp";
     }
+
+    @RequestMapping("/add")
+    public String add(HttpServletRequest request, HttpSession session){
+        int num1= Integer.parseInt(request.getParameter("num1"));
+        int num2= Integer.parseInt(request.getParameter("num2"));
+
+        int result= num1+num2;
+
+        session.setAttribute("result",result);
+        System.out.println(result);
+        return "result.jsp";
+    }
+
+
 }
---
+++ Bhaskar_Code/SpringProject/SprintBootWebMVC/src/main/webapp/result.jsp
@@ -0,0 +1,11 @@
+<%@page language="java" %>
+<HTML>
+<head>
+<link rel="stylesheet"  type="text/css" href="style.css" />
+</head>
+    <Body>
+        <h2>Result : <%= session.getAttribute("result") %></h2>
+        <h2>Result : ${result}</h2>
+
+    </Body>
+</HTML>
