1f08213cd51eb8c7781e38d3df41e826f10d39dd
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-29 20:47:21 +0530

    adding view all jobs post

--- Bhaskar_Code/JobPortal/backend/JobPortal/src/main/java/com/bhaskar/job/controllers/JobController.java
+++ Bhaskar_Code/JobPortal/backend/JobPortal/src/main/java/com/bhaskar/job/controllers/JobController.java
@@ -4,6 +4,7 @@ import com.bhaskar.job.JobService;
 import com.bhaskar.job.model.JobPost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,12 @@ public class JobController {
     public String handleForm(JobPost post){
         service.addJob(post);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewAllJobs(Model model){
+        model.addAttribute("jobPosts",service.getAllJobs());
+
+        return "viewalljobs";
     }
 }
