e7cb3ad02ce54e36f720b4dd6ac710fae975d993
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2025-12-30 17:06:22 +0530

    Response entity and exceptional handling

--- Bhaskar_Code/Microservice/quizapp/src/main/java/com/bhaskar/microservice/quizapp/controller/QuestionController.java
+++ Bhaskar_Code/Microservice/quizapp/src/main/java/com/bhaskar/microservice/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.bhaskar.microservice.quizapp.controller;
 import com.bhaskar.microservice.quizapp.entity.Question;
 import com.bhaskar.microservice.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +16,17 @@ public class QuestionController {
     private QuestionService service;
 
    @GetMapping("/allQuestions")
-    public List<Question> getAllQuestion(){
+    public ResponseEntity<List<Question>> getAllQuestion(){
         return service.getAllQuestion();
     }
 
     @GetMapping("/category/{category}")
-    public List<Question> getQuestionByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionByCategory(@PathVariable String category){
         return service.getQuestionByCategory(category);
     }
 
     @PostMapping("/add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return service.addQuestion(question);
 
     }
--- Bhaskar_Code/Microservice/quizapp/src/main/java/com/bhaskar/microservice/quizapp/service/QuestionService.java
+++ Bhaskar_Code/Microservice/quizapp/src/main/java/com/bhaskar/microservice/quizapp/service/QuestionService.java
@@ -3,8 +3,11 @@ package com.bhaskar.microservice.quizapp.service;
 import com.bhaskar.microservice.quizapp.dao.QuestionDao;
 import com.bhaskar.microservice.quizapp.entity.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,16 +17,30 @@ public class QuestionService {
     private QuestionDao repository;
 
 
-    public List<Question> getAllQuestion() {
-        return  repository.findAll();
+    public ResponseEntity<List<Question>> getAllQuestion() {
+        try{
+            return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+        e.printStackTrace();
+        }
+        return  new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionByCategory(String category) {
-        return  repository.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionByCategory(String category) {
+
+        try{
+            return new ResponseEntity<>(repository.findByCategory(category), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
-    public String addQuestion(Question question) {
+    public ResponseEntity<String> addQuestion(Question question) {
+
         repository.save(question);
-        return  "success";
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+
     }
 }
