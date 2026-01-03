e4d6fdb4ff285de3ef50e9004f61f62734bb83be
Bhaskar Mudaliyar <mudaliyarbhaskar29@gmail.com>
2026-01-03 13:35:24 +0530

    Spring boot Ecommerce Starter project

---
+++ Bhaskar_Code/SpringProject/SpringEcomProjectSpringBootAndReact/Spring-Ecommerce/src/test/java/com/bhaskar/ecom/SpringEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskar.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
